5c646fb62c5cbb273fff516debdc5681b4ec4e28
Niklas Kähkönen <kahkonen.niklas@gmail.com>
2022-04-08 13:16:13 +0300

    tehtävä 1.6 tehty

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Display = (content) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>{content.feedback} {content.amount}</p>
+  )
 }
 
-export default App;
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+const Header = (text) => {
+  return (
+    <h1>{text.text}</h1>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const headers = [
+    'give feedback',
+    'statistics'
+  ]
+  
+  return (
+    <div>
+      <Header text={headers[0]} />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header text={headers[1]} />
+      <Display feedback={'good'} amount={good} />
+      <Display feedback={'neutral'} amount={neutral} />
+      <Display feedback={'bad'} amount={bad} />
+    </div>
+  )
+}
+
+export default App
