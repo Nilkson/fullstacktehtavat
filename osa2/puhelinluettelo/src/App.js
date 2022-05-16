c808fea09aecdf9559e4ff0cff5bada69c1aba21
Niklas Kähkönen <kahkonen.niklas@gmail.com>
2022-05-16 14:36:15 +0300

    2.9 kesken

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,43 +2,71 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
-  console.log(persons);
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
       setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNumber("")
     }
 
   }
 
-  const handleInputChange = (event) => {
-    console.log(event.target.value)
+  const personstoShow = filter === ""
+  ? persons
+  : persons.filter(person => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase))
+
+  console.log(persons[0].name.toLocaleLowerCase().includes(filter));
+  console.log("vertailu", persons[0].name.localeCompare(filter, 'sv', {sensitivity: 'accent'}));
+  const handleFilterChange = (event) => {
+    // console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
+  const handlePersonChange = (event) => {
+    // console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    // console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+    filter shown with <input value={filter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2><ul>
-        {persons.map(person => <li key={person.name}> {person.name} </li>)}
+        {personstoShow.map(person => <li key={person.name}> {person.name} {person.number}</li>)}
       </ul>
     </div>
   )
