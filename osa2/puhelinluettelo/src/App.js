b081662b1b55edd7795599ef0b0f7b26830f6f85
Niklas Kähkönen <kahkonen.niklas@gmail.com>
2022-05-16 13:36:58 +0300

    tehty tehtävä 2.7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,22 +3,43 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
+  console.log(persons);
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const personObject = {
+      name: newName
+    }
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName("")
+    }
+
+  }
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <h2>Numbers</h2><ul>
+        {persons.map(person => <li key={person.name}> {person.name} </li>)}
+      </ul>
     </div>
   )
 
